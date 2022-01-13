0a9315106ce0087fee0211bff46ff5e9393ce416
NimBlemations <78566097+NimBlemations@users.noreply.github.com>
2022-01-13 01:24:21 -0700

    My eyes :)

--- src/Render3D.java
+++ src/Render3D.java
@@ -7,14 +7,16 @@ public class Render3D extends Render {
 	
 	public void floor() {
 		for(int y = 0; y < height; y++) {
-			double yDepth = y - height / 2;
-			double z = 100.0 / yDepth;
+			double ceiling = (y - height / 2.0) / height;
+			
+			double z = 8 / ceiling;
 			
 			for(int x = 0; x< width; x++) {
-				double xDepth = x - width / 2;
-				xDepth *= z;
-				int xx = (int) (xDepth);
-				pixels[x + y * width] = xx * 256;
+				double depth = (x - width / 2.0) / height;
+				depth *= z;
+				int xx = (int) (depth) & 15;
+				int yy = (int) (z) & 15;
+				pixels[x + y * width] = (xx * 16) + (yy * 16) << 8;
 			}
 		}
 	}
