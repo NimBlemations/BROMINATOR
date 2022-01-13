5ba8cb42a59645e5338d849091089d4bf294ffc2
NimBlemations <78566097+NimBlemations@users.noreply.github.com>
2022-01-13 01:26:18 -0700

    Prespective correction

--- src/Render3D.java
+++ src/Render3D.java
@@ -10,13 +10,12 @@ public class Render3D extends Render {
 			double ceiling = (y - height / 2.0) / height;
 			
 			double z = 8 / ceiling;
-			
-			for(int x = 0; x< width; x++) {
+			for(int x = 0; x < width; x++) {
 				double depth = (x - width / 2.0) / height;
 				depth *= z;
 				int xx = (int) (depth) & 15;
 				int yy = (int) (z) & 15;
-				pixels[x + y * width] = (xx * 16) + (yy * 16) << 8;
+				pixels[x + y * width] = (xx * 16) | (yy * 16) << 8;
 			}
 		}
 	}
